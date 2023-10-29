73cf4d4662024a39837f1acbf717f0b726d72281
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-10-29 23:22:08 +0100

    added cars array to page favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,20 @@
+import { CarsList } from 'components/CarsList/CarsList';
+import { useSelector } from 'react-redux';
+import { selectFavorite } from 'redux/favorite/favoriteSelector';
+
 const Favorites = () => {
-  return <div>Favorites page</div>;
+  const favoriteCars = useSelector(selectFavorite);
+
+  return (
+    <div>
+      <h1>Your favorites cars</h1>
+      {favoriteCars.length > 0 ? (
+        <CarsList data={favoriteCars} />
+      ) : (
+        <p>No cars yet</p>
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
