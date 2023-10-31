f0455b901db92605d78eaecf35e46e6bb9535cd3
Edouena8 <103317047+Edouena8@users.noreply.github.com>
2023-10-31 13:54:24 +0000

    Deploying to gh-pages from @ Edouena8/car-rental-test@58a92d2f2fa81946c9f1d5c7e38e0a55bc563329 🚀

--- static/js/395.e7cc2574.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental_test=self.webpackChunkcar_rental_test||[]).push([[395],{7395:function(e,r,t){t.r(r);var a=t(2367),s=t(9434),n=t(1930),c=t(184);r.default=function(){var e=(0,s.v9)(n.e).favoriteCars;return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"Your favorites cars"}),e.length>0?(0,c.jsx)(a.H,{data:e}):(0,c.jsx)("p",{children:"No cars yet"})]})}}}]);
-//# sourceMappingURL=395.e7cc2574.chunk.js.map
