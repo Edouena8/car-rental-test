bd526afb6a5add142f7a5b6a9bf0671189718afd
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-11-04 23:49:50 +0100

    corrected

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { persistor, store } from './redux/store';
 import './styles/fonts.css';
-import './styles/variables.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@emotion/react';
